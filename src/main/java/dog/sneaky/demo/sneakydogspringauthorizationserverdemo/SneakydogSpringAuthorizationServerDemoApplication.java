1848894e6c954ce979c92a8ccddfe3c1a0a2c918
fuzhou.dong <sneakydog@yeah.net>
2022-12-01 20:53:45 +0800

    first commit

---
+++ src/main/java/dog/sneaky/demo/sneakydogspringauthorizationserverdemo/SneakydogSpringAuthorizationServerDemoApplication.java
@@ -0,0 +1,13 @@
+package dog.sneaky.demo.sneakydogspringauthorizationserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakydogSpringAuthorizationServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneakydogSpringAuthorizationServerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dog/sneaky/demo/sneakydogspringauthorizationserverdemo/SneakydogSpringAuthorizationServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dog.sneaky.demo.sneakydogspringauthorizationserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakydogSpringAuthorizationServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
