b5b8d0f2215fab18fd439fee176751ae3d22c5a1
dfz <sneakydog@yeah.net>
2023-01-25 01:02:30 +0800

    test

---
+++ src/main/java/dog/sneaky/demo/common/CustomDefaultHandlerExceptionResolver.java
@@ -0,0 +1,6 @@
+package dog.sneaky.demo.common;
+
+import org.springframework.web.servlet.mvc.support.DefaultHandlerExceptionResolver;
+
+public class CustomDefaultHandlerExceptionResolver extends DefaultHandlerExceptionResolver {
+}
--- src/main/java/dog/sneaky/demo/configuration/ApplicationConfiguration.java
+++ src/main/java/dog/sneaky/demo/configuration/ApplicationConfiguration.java
@@ -1,21 +1,23 @@
 package dog.sneaky.demo.configuration;
 
 
+import dog.sneaky.demo.common.CustomDefaultHandlerExceptionResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
+import java.util.List;
+
 
 @Configuration
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-
 public class ApplicationConfiguration {
-
 
 
     @Bean
@@ -37,7 +39,13 @@ public class ApplicationConfiguration {
             public void addInterceptors(InterceptorRegistry registry) {
                 registry.addInterceptor(localeChangeInterceptor());
             }
+
+            @Override
+            public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+                resolvers.add(0, new CustomDefaultHandlerExceptionResolver());
+            }
         };
+
     }
 
 
